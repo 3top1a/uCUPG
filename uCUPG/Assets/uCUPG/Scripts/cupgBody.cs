5136432f57514f1bcb1dc35e1c80d1e8c15d9b7d
3top1a <57371001+3top1a@users.noreply.github.com>
2019-11-08 13:48:00 +0100

    added scripts imported from my other project

---
+++ uCUPG/Assets/uCUPG/Scripts/cupgBody.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Copyright (c) 2019 3top1a
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions
+*/
+
+
+public class cupgBody : MonoBehaviour
+{
+    //The title in the inspector 
+    [Header("Physics properties")]
+    //Mass of the object 
+    public float mass = 1;
+
+    //Density of the object (if zero, expect kraken)
+    public float density = 1;
+
+    //How bouncy the object is  
+    public float bounciness = .6f;
+
+    //Static friction of the object
+    public float staticFriction = .6f;
+
+    //A space only visible in the inspector
+    [Space]
+
+    //The force the object will have on creation 
+    public Vector3 InitForce = Vector3.zero;
+
+    //This script uses the rigidbody component
+    private Rigidbody rb;
+
+    //Time in air the object has. Detection made poorly.
+    private float timeInAir = 1;
+
+    //Refrence to the Gravity Manager
+    private cupgManager cm;
+
+    void Start()
+    {
+        cm = FindObjectOfType<cupgManager>().GetComponent<cupgManager>();
+
+        rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.mass = mass;
+        rb.SetDensity(density);
+        rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+        rb.AddForce(InitForce * cm.g * cm.gravity);
+
+        if(this.GetComponent<Collider>() != null){
+            PhysicMaterial pm = new PhysicMaterial();
+            pm.bounciness = bounciness;
+            pm.staticFriction = staticFriction;
+            gameObject.GetComponent<Collider>().material = pm;
+        }
+    }
+    
+    void FixedUpdate()
+    {
+        if (CheckIfGrounded())
+            timeInAir = 1;
+        else
+            timeInAir += Time.fixedDeltaTime / cm.timeDevider;
+        
+
+        //If not stable (eg. On ground)
+        //if(rb.)
+        //{
+            rb.AddForce(cm.gDir * cm.g * cm.gravity * mass * timeInAir);
+        //}
+    }
+
+    bool CheckIfGrounded()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, cm.gDir , out hit)){
+            if(hit.distance < (transform.localScale.y / 2) + .1f){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if(GetComponent<Rigidbody>() != null)
+        {
+            Gizmos.color = Color.blue;
+            //Gizmos.DrawSphere(rb.centerOfMass, 0.5f);
+            Gizmos.DrawSphere(rb.worldCenterOfMass, 0.5f);
+        }
+
+        if(InitForce != Vector3.zero){
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, (transform.position + InitForce));
+        }
+    }
+}
---
+++ uCUPG/Assets/uCUPG/Scripts/cupgManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cupgManager : MonoBehaviour
+{
+    //I.. I really dont know why 2
+    //Pls someone fix my equation 
+    public float g = 10;
+    public float gravity = 9.8f;
+
+    //Direction of gravity
+    public Vector3 gDir = -Vector3.up;
+
+    //Controlls how much the timeInAir var will change
+    public float timeDevider = 5;
+}
